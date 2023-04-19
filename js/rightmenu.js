21c71a13504346e8edb03bd9d8175f9ac4235f99
riy <riy@gmail.com>
2023-04-19 14:27:18 +0800

    update

--- js/rightmenu.js
+++ js/rightmenu.js
@@ -1,19 +1,19 @@
+myiniter = function() {
+    
 rm = {};
 selectTextNow = "";
-// 切换<body>元素的no-animation类
-document.body.classList.remove("no-animation");
-btf.snackbarShow("提示：已开启所有动画 GPU 狂啸");
-$(function () {
+
+
     
 // 初始化函数
 
 
 //禁止图片拖拽
-let imgElements = document.getElementsByTagName("img");
+imgElements = document.getElementsByTagName("img");
 for (let i = 0; i < imgElements.length; i++) {
-    imgElements[i].addEventListener("dragstart", function(event) {
+    imgElements[i].ondragstart = function(event) {
         event.preventDefault();
-    });
+    };
 }
 
 // 显示菜单
@@ -53,9 +53,9 @@ rm.reloadrmSize = function() {
 ;
 
 // 获取点击的href
-let domhref = "";
-let domImgSrc = "";
-let globalEvent = null;
+domhref = "";
+domImgSrc = "";
+globalEvent = null;
 
 // 监听右键初始化
 window.oncontextmenu = function(event) {
@@ -267,19 +267,19 @@ rm.copyUrl = function(id) {
 function stopMaskScroll() {
     if (document.getElementById("rightmenu-mask")) {
         let xscroll = document.getElementById("rightmenu-mask");
-        xscroll.addEventListener("mousewheel", function(e) {
+        xscroll.onmousewheel = function(e) {
             //阻止浏览器默认方法
             rm.hideRightMenu();
             // e.preventDefault();
-        }, false);
+        }, false;
     }
     if (document.getElementById("rightMenu")) {
         let xscroll = document.getElementById("rightMenu");
-        xscroll.addEventListener("mousewheel", function(e) {
+        xscroll.onmousewheel = function(e) {
             //阻止浏览器默认方法
             rm.hideRightMenu();
             // e.preventDefault();
-        }, false);
+        }, false;
     }
 }
 
@@ -341,133 +341,139 @@ rm.copyLink = function() {
 
 function addRightMenuClickEvent() {
     // 添加点击事件
-    document.getElementById("menu-backward").addEventListener("click", function() {
+    document.getElementById("menu-backward").onclick= function() {
         window.history.back();
         rm.hideRightMenu();
-    });
+    };
 
-    document.getElementById("menu-forward").addEventListener("click", function() {
+    document.getElementById("menu-forward").onclick= function() {
         window.history.forward();
         rm.hideRightMenu();
-    });
+    };
 
-    document.getElementById("menu-refresh").addEventListener("click", function() {
+    document.getElementById("menu-refresh").onclick= function() {
         window.location.reload();
-    });
+    };
 
-    document.getElementById("menu-top").addEventListener("click", function() {
+    document.getElementById("menu-top").onclick= function() {
         defaulter.scrollToDest(0, 500);
         rm.hideRightMenu();
-    });
+    };
 
     const menuLinks = document.querySelectorAll(".menu-link");
     menuLinks.forEach(function(link) {
-        link.addEventListener("click", rm.hideRightMenu);
+        link.onclick = rm.hideRightMenu;
     });
 
-    document.getElementById("menu-darkmode").addEventListener("click", rm.switchDarkMode);
+    document.getElementById("menu-darkmode").onclick = rm.switchDarkMode;
 
-    document.getElementById("menu-home") && document.getElementById("menu-home").addEventListener("click", function() {
+    document.getElementById("menu-home") && (document.getElementById("menu-home").onclick = function() {
         window.location.href = window.location.origin;
     });
 
-    document.getElementById("rightmenu-mask").addEventListener("click", rm.hideRightMenu);
+    document.getElementById("rightmenu-mask").onclick = rm.hideRightMenu;
 
-    document.getElementById("rightmenu-mask").addEventListener("contextmenu", function(event) {
+    document.getElementById("rightmenu-mask").oncontextmenu = function(event) {
         rm.hideRightMenu();
         event.preventDefault();
         // Prevent the default context menu from appearing
-    });
+    };
 
-    document.getElementById("menu-copy").addEventListener("click", rm.copyPageUrl);
+    document.getElementById("menu-copy").onclick = rm.copyPageUrl;
 
-    document.getElementById("menu-pastetext").addEventListener("click", rm.pasteText);
+    document.getElementById("menu-pastetext").onclick =  rm.pasteText;
 
-    document.getElementById("menu-copytext").addEventListener("click", function() {
+    document.getElementById("menu-copytext").onclick= function() {
         try{
         rm.rightmenuCopyText(selectTextNow);
         btf.snackbarShow("复制成功，快去粘贴吧！");
     } catch (err) {
         btf.snackbarShow("复制失败，没有权限！");
     }
-    });
+    };
 
-    document.getElementById("menu-newwindow").addEventListener("click", function() {
+    document.getElementById("menu-newwindow").onclick= function() {
         window.open(domhref);
         rm.hideRightMenu();
-    });
+    };
 
-    document.getElementById("menu-copylink").addEventListener("click", rm.copyLink);
+    document.getElementById("menu-copylink").onclick =  rm.copyLink;
 
-    document.getElementById("menu-downloadimg").addEventListener("click", function() {
+    document.getElementById("menu-downloadimg").onclick= function() {
         defaulter.downloadImage(domImgSrc);
-    });
+    };
 
-    document.getElementById("menu-newwindowimg").addEventListener("click", function() {
+    document.getElementById("menu-newwindowimg").onclick= function() {
         window.open(domImgSrc);
         rm.hideRightMenu();
-    });
+    };
 
-    document.getElementById("menu-copyimg").addEventListener("click", function() {
+    document.getElementById("menu-copyimg").onclick= function() {
         rm.writeClipImg(domImgSrc);
-    });
+    };
 
-    document.getElementById("menu-searchBing").addEventListener("click", rm.searchBaidu);
+    document.getElementById("menu-searchBing").onclick =  rm.searchBaidu;
 
     //音乐
-    document.getElementById("menu-music-toggle").addEventListener("click", defaulter.musicToggle);
+    document.getElementById("menu-music-toggle").onclick =  defaulter.musicToggle;
 
-    document.getElementById("menu-music-back").addEventListener("click", defaulter.musicSkipBack);
+    document.getElementById("menu-music-back").onclick =  defaulter.musicSkipBack;
 
-    document.getElementById("menu-music-forward").addEventListener("click", defaulter.musicSkipForward);
+    document.getElementById("menu-music-forward").onclick =  defaulter.musicSkipForward;
 
-    document.getElementById("menu-music-copyMusicName").addEventListener("click", function() {
+    document.getElementById("menu-music-copyMusicName").onclick= function() {
         try{
         rm.rightmenuCopyText(defaulter.musicGetName());
         btf.snackbarShow("复制歌曲名称成功");
     } catch (err) {
         btf.snackbarShow("复制失败，没有权限！");
     }
-    });
+    };
 
-    GLOBAL_CONFIG.Snackbar.position = "bottom-center"
-
-    // 获取<body>元素
-    var bodyElement = document.body;
-
-    // 获取用于控制动画的按钮
-    var controlButton = document.getElementById("menu-css-stop");
-    var toggleButton = document.getElementById("menu-css-toggle");
-    // 定义一个函数，用于更新按钮的文本
-    function updateButtonText() {
-        if (bodyElement.classList.contains("no-animation")) {
-            toggleButton.textContent = "开启所有动画";
-            btf.snackbarShow("系统设置：已暂停所有动画 GPU 狂笑");
-        } else {
-            toggleButton.textContent = "暂停所有动画";
-            btf.snackbarShow("提示：已开启所有动画 GPU 狂啸");
-        }
-    }
-
-
-    // 初始化按钮的文本
-    updateButtonText();
-
-    // 为按钮添加点击事件监听器
-    controlButton.addEventListener("click", function() {
-        // 切换<body>元素的no-animation类
-        bodyElement.classList.toggle("no-animation");
-        // 更新按钮的文本
-        updateButtonText();
-        rm.hideRightMenu();
-    });
 
 
 }
 
+GLOBAL_CONFIG.Snackbar.position = "bottom-center"
 
 
+
+
+// 获取<body>元素
+bodyElement = document.body;
+
+// 获取用于控制动效的按钮
+controlButton = document.getElementById("menu-css-stop");
+toggleButton = document.getElementById("menu-css-toggle");
+// 定义一个函数，用于更新按钮的文本
+function updateButtonText() {
+    if (bodyElement.classList.contains("no-animation")) {
+        toggleButton.textContent = "开启所有动效";
+        btf.snackbarShow("系统设置：已暂停所有动效 GPU 狂笑");
+    } else {
+        toggleButton.textContent = "暂停所有动效";
+        btf.snackbarShow("提示：已开启所有动效 GPU 狂啸");
+    }
+}
+
+// 切换<body>元素的no-animation类
+document.body.classList.remove("no-animation");
+// 更新按钮的文本
+updateButtonText();
+
+// 为按钮添加点击事件监听器
+controlButton.onclick= function() {
+    // 切换<body>元素的no-animation类
+    bodyElement.classList.toggle("no-animation");
+    // 更新按钮的文本
+    updateButtonText();
+    rm.hideRightMenu();
+};
 
 addRightMenuClickEvent();
-btf.snackbarShow("提示：已开启所有动画 GPU狂啸（右键菜单可关闭）",);
+
+}
+
+$(function () {
+    myiniter()
 })
